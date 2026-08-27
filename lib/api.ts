aa298537c58168c9e525f58978bfaa2e8b7ba68f
Angeles <delosangelesgonzalez@gmail.com>
2026-08-27 18:49:59 +0200

    Create api.ts to handle the calls to the api server.

---
+++ lib/api.ts
@@ -0,0 +1,27 @@
+import { House } from './types';
+
+export async function getHouses(): Promise<House[]> {
+
+    const res = await fetch(`${process.env.API_URL}/api/houses`, {
+        next: { revalidate: 3600 },
+    });
+
+    if (!res.ok) {
+        throw new Error(`There's been an error while fetching the houses, check the api.`)
+    }
+
+    return res.json();
+
+}
+
+export async function getHouseByID(id: string): Promise<House | undefined> {
+    const res = await fetch(`${process.env.API_URL}/api/houses/${id}`, {
+        next: { revalidate: 3600 },
+    });
+
+    if (!res.ok) {
+        throw new Error(`There's been an error while fetching the house, check the api or the id.`)
+    }
+
+    return res.json();
+}
